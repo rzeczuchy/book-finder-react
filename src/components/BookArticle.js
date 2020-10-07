e1d1dadf50fc84d5343fbe8aba12f53e38391595
rzeczuchy <57368722+rzeczuchy@users.noreply.github.com>
2020-10-07 13:14:46 +0200

    building project structure, adding styles, testing component import

---
+++ src/components/BookArticle.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const BookArticle = (props) => {
+    return (
+        <article className="book">
+            <img src="" alt="Book cover"></img>
+            <h1>Book title</h1>
+            <p>Book description</p>
+        </article>
+    );
+}
+
+export default BookArticle;
---
+++ src/components/index.js
@@ -0,0 +1,1 @@
+export { default as BookArticle } from "./BookArticle";
