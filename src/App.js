96e2f502e08c83193e8673f50d2724755acc989b
rzeczuchy <57368722+rzeczuchy@users.noreply.github.com>
2020-10-07 13:20:26 +0200

    minor cleanup, adding Google Fonts

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BookArticle } from "./components";
 import "./css/index.css";
 
-function App() {
+const App = (props) => {
   return (
     <>
       <BookArticle />
