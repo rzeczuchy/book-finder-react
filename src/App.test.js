974598f77c8451ff624da74915fec1fee3b3c97d
rzeczuchy <57368722+rzeczuchy@users.noreply.github.com>
2020-10-09 12:46:22 +0200

    adding basic tests for SearchForm

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { render } from "@testing-library/react";
 import App from "./App";
 import { shallow } from "enzyme";
+import { BookList, SearchForm } from "./components";
 
-test("app renders succesfully", () => {
+describe("App", () => {
+  test("app renders (sanity check)", () => {
     shallow(<App />);
+  });
+});
+
+describe("SearchForm", () => {
+  test("search form renders inputs", () => {
+    const form = shallow(<SearchForm />);
+    const titleInput = <input name="title" />;
+    const authorInput = <input name="author" />;
+    expect(form.containsMatchingElement(titleInput)).toBeTruthy();
+    expect(form.containsMatchingElement(authorInput)).toBeTruthy();
+  });
+
+  test("search form renders submit button", () => {
+    const form = shallow(<SearchForm />);
+    const searchButton = <button>Submit</button>;
+    expect(form.containsMatchingElement(searchButton)).toBeTruthy();
+  });
 });
