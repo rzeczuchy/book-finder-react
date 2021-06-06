3bf7bf998e84a470a6b3a8a19962c3b0505b46ef
rzeczuchy <57368722+rzeczuchy@users.noreply.github.com>
2021-06-06 15:09:44 +0200

    moving api request functions to helpers

--- src/components/helpers.js
+++ src/components/helpers.js
@@ -21,3 +21,31 @@ export const joinArray = (arr, maxValues, undefMessage) => {
 export const replaceHttp = (url) => {
   return typeof url === "string" ? url.replace(/^http:\/\//i, "https://") : "";
 };
+
+const makeBooksApiQueryUrl = (query, startIndex, maxResults) => {
+  var url = "https://www.googleapis.com/books/v1/volumes?q=";
+  url += query;
+  url += "&startIndex=" + startIndex;
+  url += "&maxResults=" + maxResults;
+  url += "&key=";
+  url += process.env.REACT_APP_GOOGLE_API_KEY;
+  return url;
+};
+
+export const sendRequest = (query, startIndex, maxResults, onLoad) => {
+  const request = new XMLHttpRequest();
+  const url = makeBooksApiQueryUrl(query, startIndex, maxResults);
+  request.open("GET", url, true);
+  request.onload = (e) => {
+    onLoad(request);
+  }
+  request.send();
+}
+
+export const requestSuccessful = (request) => {
+  return request.readyState === 4 && request.status === 200;
+}
+
+export const apiResponseJson = (request) => {
+  return JSON.parse(request.response);
+}
